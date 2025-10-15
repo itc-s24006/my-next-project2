6926c4f84a1d17ef49f93f7ae63c9f746731cfaa
itc-s24006 <s24006@std.it-college.ac.jp>
2025-10-15 12:36:34 +0900

    p141 microCMS側の準備・メンバーリスト取得の関数作成まで　完了

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -1,3 +1,17 @@
+import { createClient } from "microcms-js-sdk";
+import type {
+  MicroCMSQueries,
+  MicroCMSImage,
+  MicroCMSListContent,
+} from "microcms-js-sdk";
+
+export type Member = {
+  name: string;
+  position: string;
+  profile: string;
+  image: MicroCMSImage;
+} & MicroCMSListContent;
+
 export type Category = {
   name: string;
 };
@@ -11,3 +25,29 @@ export type News = {
   publishedAt: string;
   createdAt: string;
 };
+
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+  throw new Error("MICROCMS_SERVICE_DOMAIN is required");
+}
+
+if (!process.env.MICROCMS_API_KEY) {
+  throw new Error("Missing MICROCMS_API_KEY is required");
+}
+
+//createClient    sdkに用意されている関数。クライアントを作成する
+const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
+});
+
+// メンバー一覧を取得する関数
+export const getMemberList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<Member>({
+    endpoint: "members",
+    queries,
+  });
+  return listData;
+};
+/* MicroCMSQueries  microCMSが提供するクエリパラメータの型情報
+  getList           microCMSから一覧データを取得するためのメソッド。クライアントで用意されている。
+*/
