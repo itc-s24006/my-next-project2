079c88a094af4aafc9698a0f0686732849fb0730
itc-s24006 <s24006@std.it-college.ac.jp>
2025-10-16 09:57:34 +0900

    p146 セクション5-3 まで完了

---
+++ app/_constants/index.ts
@@ -0,0 +1,2 @@
+// 定数の管理
+export const MEMBERS_LIST_LIMIT = 100;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // 画像の外部ドメイン許可設定 microCMSの画像を許可
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.microcms-assets.io",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
